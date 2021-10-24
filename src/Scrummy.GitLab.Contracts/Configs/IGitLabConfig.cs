a7d6259df8fa6e9d1863abf404c4f70c4ed27b0e
Christian Juenger <christianjuenger1987@gmail.com>
2021-10-24 18:20:47 +0200

    refactor: removes redundant property

--- src/Scrummy.GitLab.Contracts/Configs/IGitLabConfig.cs
+++ src/Scrummy.GitLab.Contracts/Configs/IGitLabConfig.cs
@@ -1,11 +1,7 @@
-﻿using IO.Juenger.GitLab.Client;
-
-namespace Scrummy.GitLab.Contracts.Configs
+﻿namespace Scrummy.GitLab.Contracts.Configs
 {
     public interface IGitLabConfig
     {
-        Configuration RestApiConfig { get; set; }
-
         string BacklogLabel { get; set; }
     }
 }
--- src/Scrummy.GitLab/Configs/GitLabConfig.cs
+++ src/Scrummy.GitLab/Configs/GitLabConfig.cs
@@ -1,12 +1,9 @@
-﻿using IO.Juenger.GitLab.Client;
-using Scrummy.GitLab.Contracts.Configs;
+﻿using Scrummy.GitLab.Contracts.Configs;
 
 namespace Scrummy.GitLab.Configs
 {
     public class GitLabConfig : IGitLabConfig
     {
-        public Configuration RestApiConfig { get; set; }
-
         public string BacklogLabel { get; set; } = "Backlog";
     }
 }
